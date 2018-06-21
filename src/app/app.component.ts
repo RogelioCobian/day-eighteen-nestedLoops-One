4503607dcb31c03a6d481cd71bfed7643bcc797a
Rogelio Cobian <rogeliocobian2@gmail.com>
2018-06-21 15:54:35 -0700

    created nested arrays and a typeOf method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  bools: Array<any> = [
+    true,
+    false
+  ];
+
+  nums: any[] = [
+    1,
+    2.5,
+    5
+  ];
+
+  strs: Array<any> = [
+    'Hello',
+    'There'
+  ];
+
+  test: any =  {
+    id: 2,
+    name: 'user'
+  };
+
+  user: any = {
+    id: 2,
+    name: 'user'
+  };
+
+  obj: any = [
+    this.test,
+    this.user
+  ];
+
+  types: Array<any> = [
+    this.bools,
+    this.nums,
+    this.strs,
+    this.obj
+  ];
+
+
+  getTypeOf(x) {
+    return typeof(x);
+  }
+
+  ngOnInit() {
+    console.log(this.types[0]);
+  }
 }
+
+
+// First Attempt is right but wanted to check the other way
+        // types: Array<any> = [ {
+        //   // user : {
+        //   //   id: 2,
+        //   //   name: 'user'
+        //   // },
+
+        //   // bools: [
+        //   //   true,
+        //   //   false
+        //   // ],
+
+        //   // nums: [
+        //   //   1,
+        //   //   2.5,
+        //   //   5
+        //   // ],
+
+        //   // strs: [
+        //   //   'Hi',
+        //   //   'There'
+        //   // ],
+
+        //   // objs: [{
+        //   //   test : {
+        //   //   id: 1,
+        //   //   name: 'test'
+        //   // }
+        //   // }]
+
+        // }];
